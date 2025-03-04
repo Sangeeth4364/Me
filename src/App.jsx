4e62a6dc148698e8608b3771c3dd55afd6223911
sangeeth <iamsangeeths@gmail.com>
2025-03-04 11:42:05 +0530

    Fixed image path issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <div className="h-auto w-full">
+        <div className="h-100vh w-full bg-gray-200">
           <img src={Bike1} alt="Bike1" />
         </div>
       </div>
